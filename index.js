24bf0e7e5391d66276bff19ffab3847adb38c808
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-24 14:33:18 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+//PLUGINS
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const prefix = ">";
+const bot = new commando.Client({
+    commandPrefix: prefix
+});
+//PLUGINS
+
+//BOT TOKEN
+bot.login(process.evn.token);
+//BOT TOKEN
+
+//GETS THE BOT ONLINE
+bot.on('ready',function(){
+    console.log(`Bot is now online, being kyzer's slave!`);
+    bot.user.setActivity(`>help | KYZER`, { type: 'WATCHING' });
+});
+//GETS THE BOT ONLINE
+
+//REGISTRIES
+bot.registry.registerGroup('kyzerpvp', 'kyzerpvp')
+bot.registry.registerCommandsIn(__dirname + "/commands");
+bot.registry.registerDefaults();
+//REGISTIES
+
