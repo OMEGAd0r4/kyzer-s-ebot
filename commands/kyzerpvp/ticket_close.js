9df2c2f1515be1cd5c38b10f5f6baa3fa9055cb7
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-25 14:45:46 +0700

    Create ticket_close.js

---
+++ commands/kyzerpvp/ticket_close.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = ">";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class closeCommand extends commando.Command {
+    constructor(client) 
+    {
+      super(client, {
+        name: 'close', 
+        group: 'kyzerpvp',
+        memberName: 'close',
+        description: "killed kyzer. GOOD JOB"
+      });
+    }
+
+    async run(message, args)
+    {
+    message.channel.send(`Are you sure you 28 potted kyzer? Once confirmed, you cannot reverse this action!\nTo confirm, type \`>confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '>confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Kyzer injected vape and now t-baging on ur dead nan').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+    }
+}
+
+module.exports = closeCommand;
