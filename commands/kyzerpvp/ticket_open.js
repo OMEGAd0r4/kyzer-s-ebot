5099ea23a6e600b0e80208490f4dcf8c793489c1
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-25 14:55:24 +0700

    Update ticket_open.js

--- commands/kyzerpvp/ticket_open.js
+++ commands/kyzerpvp/ticket_open.js
@@ -9,16 +9,16 @@ class kyzer1v1Command extends commando.Command {
     constructor(client) 
     {
       super(client, {
-        name: 'kyzer1v1', 
+        name: 'kyzerpm', 
         group: 'kyzerpvp',
-        memberName: 'kyzer1v1',
-        description: "1v1 KYZER RN. KYZER IS EZ. GO GET HIM"
+        memberName: 'kyzerpm',
+        description: "Got talk privately to kyzah"
       });
     }
     async run(message, args)
     {       
-        if (message.guild.channels.exists("name", "1v1 " + message.author.id)) return message.channel.send("You already have a ticket open.");
-        message.guild.createChannel(`1v1 ${message.author.id}`, "text").then(c => {
+        if (message.guild.channels.exists("name", "pmroom " + message.author.id)) return message.channel.send("You already have a pmroom open.");
+        message.guild.createChannel(`pmroom ${message.author.id}`, "text").then(c => {
             let role = message.guild.roles.find("name", "$$$");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
@@ -33,15 +33,15 @@ class kyzer1v1Command extends commando.Command {
                  SEND_MESSAGES: true,
                  READ_MESSAGES: true
             });
-            message.channel.send(`GO FIGHT KYZER HE IS EZ ASF. YOU AND KYZER HAVE SUCCESSFULLY BEEN TELEPORTED TO KYZER'S DUNGEON, ${c.name}.`);
+            message.channel.send(`Your pm room has been created, ${c.name}.`);
             const embed = new Discord.RichEmbed()
             .setColor("#000000")
-            .addField(`Hey ${message.author.username}!, Your arena has been created, you have been teleported. Pot up your speed pots and fire res, go 28 pot him`, `Thanks for your patience.`)
+            .addField(`Hey ${message.author.username}!, You can now privately talk to kyzah now!`, `Thanks for your patience.`)
             .setTimestamp();
             c.send({ embed: embed });
         }).catch(console.error);
         }
 }
 
-module.exports = kyzer1v1Command;
+module.exports = kyzerpmCommand;
 
