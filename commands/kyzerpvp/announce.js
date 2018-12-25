fc5fe9215a82096fa349a4a7a68029c1599663b9
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-25 09:06:43 +0700

    Create announce.js

---
+++ commands/kyzerpvp/announce.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const commando = require('discord.js-commando')
+const prefix = ">";
+const bot = new commando.Client({
+  commandPrefix: prefix
+});
+
+class announceCommand extends commando.Command {
+  constructor(client) 
+  {
+    super(client, {
+      name: 'announce', 
+      group: 'kyzerpvp',
+      memberName: 'announce',
+      description: "Announces a message in #announcements"
+    });
+  }
+
+  async run (message, args)
+  {
+    let announcementargs = message.content.slice(prefix.length).split(/ + /); //MAIN ARGS
+    let announcementmessage = announcementargs.join(" ").slice(9);
+
+    let announcementschannel = message.guild.channels.find(`name`, "announcements");
+    if(!announcementschannel) return message.channel.send("Couldn't find the announcements channel");
+    var supportteamrole = message.guild.roles.find(`name`, "Admin");
+    if (!message.member.roles.has(supportteamrole.id)) return message.channel.send("No admin no announcement. Stupid fak")
+
+    var announcementmessageembed = new Discord.RichEmbed()
+    .setTitle("**Lolicop | Announcement**")
+    .setColor("#FFDF00")
+    .addField("**__Announcement__**", `${announcementmessage}`)
+    .setTimestamp()
+
+  }
+}
+
+module.exports = announceCommand;
