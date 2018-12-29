c2ad4a311a366ecdc80399ae341b41e265bfa207
OMEGAd0r4 <45790881+OMEGAd0r4@users.noreply.github.com>
2018-12-29 16:32:45 +0700

    Update irl.js

--- commands/kyzerpvp/irl.js
+++ commands/kyzerpvp/irl.js
@@ -18,7 +18,7 @@ class irlCommand extends commando.Command {
 
   async run(message)
   {
-      var irls = ["https://cdn.discordapp.com/attachments/525200110049689600/526677259231297546/tlzf7otr80911.png", "https://cdn.discordapp.com/attachments/525200110049689600/526677177928777739/Z.png", "https://c8.alamy.com/comp/E0X8AN/close-up-of-little-girl-with-a-lollipop-isolated-on-white-background-E0X8AN.jpg", "https://previews.123rf.com/images/johanjk/johanjk1309/johanjk130900154/22400660-young-sensual-girl-sucking-lollipop-outdoors-lifestyle-.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOcKcgEK2Cfo-oGTJuDshdgOk-i-sHD0gmFiUEOgiyY7svuCJe"];
+      var irls = ["http://i.imgur.com/3G7ex.jpg", "https://cdn.discordapp.com/attachments/525200110049689600/526677259231297546/tlzf7otr80911.png", "https://cdn.discordapp.com/attachments/525200110049689600/526677177928777739/Z.png", "https://c8.alamy.com/comp/E0X8AN/close-up-of-little-girl-with-a-lollipop-isolated-on-white-background-E0X8AN.jpg", "https://previews.123rf.com/images/johanjk/johanjk1309/johanjk130900154/22400660-young-sensual-girl-sucking-lollipop-outdoors-lifestyle-.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOcKcgEK2Cfo-oGTJuDshdgOk-i-sHD0gmFiUEOgiyY7svuCJe"];
 
       message.channel.send({embed: new Discord.RichEmbed()
         .setTitle("**Lolicop's IRL**")
